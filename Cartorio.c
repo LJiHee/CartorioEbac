2023969461ba1428065a1678267b8cb90d4031d8
Leiry <86642497+LJiHee@users.noreply.github.com>
2023-07-28 21:40:01 -0300

    Opção de Saída
    Espaçamento corrigido no menu de opções

--- Cartorio.c
+++ Cartorio.c
@@ -122,7 +122,7 @@ int main()
 		printf("\t1 - Registrar nomes \n");
 		printf("\t2 - Consultar nomes \n");
 		printf("\t3 - Deletar nomes \n"); 
-		printf("\t4 - Sair do sistema \n");
+		printf("\t4 - Sair do sistema \n\n");
 		printf("Opção: "); //Fim do menu
 		
 		scanf("%d", &opcao); //Armazenando a escolha do usuário
